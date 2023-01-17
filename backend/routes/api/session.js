04e7aaa3e552176dc0d79e159e510c3d84c86e26
Laila <lailadenman@gmail.com>
2023-01-17 15:50:39 -0800

    Add User input validation on user login backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -3,11 +3,26 @@ const express = require('express');
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
 const router = express.Router();
+
+const validateLogin = [
+    check('credential')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage('Please provide a valid email or username.'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a password.'),
+    handleValidationErrors
+];
 
 // Log in
 router.post(
     '/',
+    validateLogin,
     async (req, res, next) => {
         const { credential, password } = req.body;
 
@@ -86,5 +101,24 @@ fetch('/api/session', {
         "XSRF-TOKEN": `<value of XSRF-TOKEN cookie>`
     }
 }).then(res => res.json()).then(data => console.log(data));
+
+fetch('/api/session', {
+    method: 'POST',
+    headers: {
+        "Content-Type": "application/json",
+        "XSRF-TOKEN": `<value of XSRF-TOKEN cookie>`
+    },
+    body: JSON.stringify({ credential: '', password: 'password' })
+}).then(res => res.json()).then(data => console.log(data));
+
+fetch('/api/session', {
+    method: 'POST',
+    headers: {
+        "Content-Type": "application/json",
+        "XSRF-TOKEN": `<value of XSRF-TOKEN cookie>`
+    },
+    body: JSON.stringify({ credential: 'Demo-lition', password: '' })
+}).then(res => res.json()).then(data => console.log(data));
+
 
 module.exports = router;
