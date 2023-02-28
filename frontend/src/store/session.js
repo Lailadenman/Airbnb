aafc400f0cf7f5db7f345dc53a60d882e09f3489
Laila <lailadenman@gmail.com>
2023-02-27 18:14:13 -0800

    Log out

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -58,6 +58,14 @@ export const signup = (user) => async (dispatch) => {
     return response;
 };
 
+export const logout = () => async (dispatch) => {
+    const response = await csrfFetch('/api/session', {
+        method: 'DELETE',
+    });
+    dispatch(removeSessionUser());
+    return response;
+};
+
 const sessionReducer = (state = initialState, action) => {
     let newState;
 
